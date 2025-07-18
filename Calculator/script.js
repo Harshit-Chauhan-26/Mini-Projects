21aabb2d92b7e3eeefb64e2554e1616a79a8ce13
Harshit Chauhan <businessharshit26@gmail.com>
2025-07-19 01:07:11 +0530

    script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,78 +1,21 @@
-const currDisplay = document.querySelector(".curr-display");
-const prevDisplay = document.querySelector(".prev-display");
-const numbers = document.querySelectorAll(".number");
-const operands = document.querySelectorAll(".operation");
-const clearBtn = document.querySelector(".clear");
-const delBtn = document.querySelector(".delete");
-const equalBtn = document.querySelector(".equal");
-let operation;
+let display = document.getElementById("display");
 
-function appendNumber(number) {
-  if (number === "." && currDisplay.innerText.includes(".")) return;
-  currDisplay.innerText += number;
-}
-
-function chooseOperation(operand) {
-  if (currDisplay.innerText === "") return;
-  compute(operand);
-  operation = operand;
-  currDisplay.innerText += operand;
-  prevDisplay.innerText = currDisplay.innerText;
-  currDisplay.innerText = "";
+function appendValue(value) {
+  display.value += value;
 }
 
 function clearDisplay() {
-  currDisplay.innerText = "";
-  prevDisplay.innerText = "";
+  display.value = "";
 }
 
-function compute(operand) {
-  let result;
-  const previousValue = parseFloat(prevDisplay.innerText);
-  const currentValue = parseFloat(currDisplay.innerText);
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
 
-  if (isNaN(previousValue) || isNaN(currentValue)) return;
-
-  switch (operation) {
-    case "+":
-      result = previousValue + currentValue;
-      break;
-    case "-":
-      result = previousValue - currentValue;
-      break;
-    case "*":
-      result = previousValue * currentValue;
-      break;
-    case "/":
-      result = previousValue / currentValue;
-      break;
-    default:
-      return;
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
   }
-  currDisplay.innerText = result;
 }
-
-numbers.forEach((number) => {
-  number.addEventListener("click", () => {
-    appendNumber(number.innerText);
-  });
-});
-
-operands.forEach((operand) => {
-  operand.addEventListener("click", () => {
-    chooseOperation(operand.innerText);
-  });
-});
-
-clearBtn.addEventListener("click", () => {
-  clearDisplay();
-});
-
-equalBtn.addEventListener("click", () => {
-  compute();
-  prevDisplay.innerText = "";
-});
-
-delBtn.addEventListener("click", () => {
-  currDisplay.innerText = currDisplay.innerText.slice(0, -1);
-});
